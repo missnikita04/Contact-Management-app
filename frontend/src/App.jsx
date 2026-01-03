a28d16536905023b220aaa657574f1581f3e5b49
Nikita Chauhan <nikitachauhan3804@gmail.com>
2026-01-03 03:00:44 -0800

    add backend url to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [contacts, setContacts] = useState([]);
 
   const fetchContacts = async () => {
-    const res = await axios.get("http://localhost:5000/api/contacts");
+    const res = await axios.get("https://contact-management-app-backend-5rvl.onrender.com/api/contacts");
     setContacts(res.data);
   };
 
